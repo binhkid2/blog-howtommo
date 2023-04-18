21a1ed0a295683c8478cc7dfcc3349954e6cda38
binhkid2 <binhkid2@gmail.com>
2023-04-18 18:10:59 +0700

    hello

---
+++ data/site.ts
@@ -0,0 +1,14 @@
+export const site = {
+  title: 'How to MMO',
+  description: 'How to make money online with chat GTP and other nocode,lowcode tools',
+  // This should be a url to the static file you're using for your open graph image
+  ogImage: 'https://res.cloudinary.com/binkk/image/upload/v1681815785/default_nwy8fr.png',
+  copyrightName: 'How to MMO',
+  // The url of the source code for the site, used in the footer.
+  viewSourceUrl: 'https://fresh.deno.dev/'
+}
+
+export const settings = {
+  showLittleSticksPlug: true,
+}
+
